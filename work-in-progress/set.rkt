033f2ee12050c7ca887430dc8f0aa50e90a5e730
frenchfrog42 <76942432+frenchfrog42@users.noreply.github.com>
2021-10-28 16:56:58 +0200

    Ok on testnet

--- work-in-progress/set.rkt
+++ work-in-progress/set.rkt
@@ -6,36 +6,40 @@
 ;(bytes->hex-string (sha256-bytes (hex-string->bytes ""))) ; OP_0 OP_SHA256
 
 (define depth 1)
+(define how-many-per-level 256) ;lol
+;todo split in 16 * 16
 
 (define contract
   ; fails if name already taken
   ; update the root of the tree
   `(public
      ; arguments ==> string value hint0-left hint0-right hint1-left ... to hint,(depth - 1)-right
-     ,(append '(string value-of-string old-root)
+     ,(append '(string value-of-string real-old-root real-new-root)
               (flatten (for/list ((i (in-range depth)))
                          (list
                           (string->symbol (~a "hint" i "-left"))
                           (string->symbol (~a "hint" i "-right"))))))
      ; code of the function
-     (define old-root (call sha256 (value-of-string))) ; computation of the old-root
-     (define new-root (call sha256 (1))) ; the new root starts from 1 (or something else (todo))
+     (define old-root (call sha256 (0))) ; computation of the old-root
+     (define new-root (call sha256 (value-of-string))) ; the new root starts from 1 (or something else (todo))
      ,(cons*
        (for/list ((i (in-range depth)))
          (cons* `(
                   ; first verify hints
-                  (define current-byte (bytes-get-first (bytes-delete-first value-of-string ,i) 1))
+                  (define current-byte (bytes-get-first (bytes-delete-first string ,i) 1))
                   ; hint0-left should be of size size(curren-byte)
-                  (verify (= (call getLen ((destroy current-byte))) (call getLen (hint0-left)))); -1 maybe
+                  (drop current-byte); (verify (= ((destroy current-byte)) (/ (call getLen (hint0-left)) 32))); -1 maybe ; mouais verif ça
                   ; modify current-hash
                   ,(let ((left (string->symbol (~a "hint" i "-left")))
                          (right (string->symbol (~a "hint" i "-right"))))
                      `(cons
                        (modify old-root (call sha256 (,(+bytes* `(,left old-root ,right)))))
-                       (modify new-root (call sha256 (,(+bytes* `(,left new-root ,right))))))))))) ;todo drop left/right
+                       (modify new-root (call sha256 (,(+bytes* `((destroy ,left) new-root (destroy ,right)))))))))))) ;todo drop left/right
      ; verification of hints
-     (define real-old-root 0) ;debug
-     (verify (= real-old-root old-root)) ;real-old-root is root parsed from state
+     ;(define real-old-root 0) ;debug
+     (verify (= real-old-root old-root)) ;ok ;real-old-root is root parsed from state
+     (verify (= real-new-root new-root))
+     (drop old-root) (drop real-old-root) (drop string) (drop new-root) (drop real-new-root);debug
      ; right now, everything is verified, we can do the computation
 
      ; for a set, we check value was something different from 0, the new value is 1
@@ -44,7 +48,7 @@
      ; we would just update the root, without any check on value-of-string for the javascript "set"
      ; and set (at the start of new-root) to either hash(0), or hash(something else) for a deletion/modification
      (verify value-of-string)
-     
+     (destroy value-of-string) ;debug
      ; update new root to the state
      ;todo
      ))
@@ -52,20 +56,27 @@
 (define first-root
   (let ((tmp (sha256-bytes (hex-string->bytes "")))) ;we start from OP_0
     (for/list ((i (in-range depth)))
-      (set! tmp (sha256-bytes (list->bytes (flatten (for/list ((_ (in-range depth))) (bytes->list tmp)))))))
+      (set! tmp (sha256-bytes (list->bytes (flatten (for/list ((_ (in-range how-many-per-level))) (bytes->list tmp)))))))
     (bytes->hex-string tmp)))
 
 (define un (sha256-bytes (hex-string->bytes "")))
 (define lun (bytes->list un))
 (bytes->hex-string un)
 
-(define deux (sha256-bytes (list->bytes (flatten (for/list ((d (in-range depth))) lun)))))
+(define deux (sha256-bytes (list->bytes (flatten (for/list ((d (in-range how-many-per-level))) lun)))))
 (define ldeux (bytes->list deux))
 (bytes->hex-string deux)
 
-(define trois (sha256-bytes (list->bytes (flatten (for/list ((d (in-range depth))) ldeux)))))
+(define trois (sha256-bytes (list->bytes (flatten (for/list ((d (in-range how-many-per-level))) ldeux)))))
 (bytes->hex-string trois)
+; first hint
+;(bytes->hex-string (list->bytes (flatten (for/list ((_ (in-range (- how-many-per-level 1)))) lun))))
+
+(bytes->hex-string
+ (sha256-bytes (list->bytes (append
+                             (flatten (for/list ((_ (in-range (- how-many-per-level 1)))) (bytes->list (sha256-bytes (hex-string->bytes "")))))
+                             (bytes->list (sha256-bytes (hex-string->bytes "01")))))))
 
 first-root
 
-;(contract->opcodes contract)
+(contract->opcodes contract)
