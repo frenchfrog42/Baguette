435931bfa4ab9dadbd4e30d2d0792c13bcc4087e
frenchfrog42 <76942432+frenchfrog42@users.noreply.github.com>
2021-11-16 13:07:03 +0100

    Fix test file

--- test/push_tx.rkt
+++ test/push_tx.rkt
@@ -19,18 +19,21 @@
     ))
 
 (define lispcoded '(
-                  (define temp-first-part "3044022079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817980220")
-                  (define my-tx tx-arg) ;copy tx arg
-                  (define my-tx-hashed (call hash256 ((destroy my-tx))))
-                  (define my-tx-hashed-plus1 (+ 1 (destroy my-tx-hashed)))
-                  (define first-part (+bytes
-                                            (destroy temp-first-part)
-                                            (destroy my-tx-hashed-plus1)))
-                  (define sighashflags "41")
-                  (define signature (+bytes (destroy first-part) (destroy sighashflags)))
-                  (define pubkey "02b405d7f0322a89d0f9f3a98e6f938fdc1c969a8d1382a2bf66a71ae74a1e83b0")
-                  (call checksigverify ((destroy signature) (destroy pubkey)))))
+                    (define temp-first-part "3044022079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817980220")
+                    (define my-tx tx-arg) ;copy tx arg
+                    (define my-tx-hashed (call hash256 ((destroy my-tx))))
+                    (define my-tx-hashed-plus1 (+ 1 (destroy my-tx-hashed)))
+                    (define first-part (+bytes
+                                        (destroy temp-first-part)
+                                        (destroy my-tx-hashed-plus1)))
+                    (define sighashflags "41")
+                    (define signature (+bytes (destroy first-part) (destroy sighashflags)))
+                    (define pubkey "02b405d7f0322a89d0f9f3a98e6f938fdc1c969a8d1382a2bf66a71ae74a1e83b0")
+                    (call checksigverify ((destroy signature) (destroy pubkey)))))
 
-(check string=?
-       (contract->opcodes (append '(public (tx-arg)) lispcoded))
-       (contract->opcodes handcoded))
+; todo faire un appel a pushtx
+
+(if (string=?
+     (contract->opcodes (append '(public (tx-arg)) lispcoded))
+     (contract->opcodes handcoded))
+    '() (exit 1))
