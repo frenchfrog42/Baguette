56cafbb8bbf0b38bcfb7652f2853f7b6abac3423
frenchfrog42 <76942432+frenchfrog42@users.noreply.github.com>
2021-11-25 19:07:32 +0100

    Update state.rkt

--- examples/state.rkt
+++ examples/state.rkt
@@ -4,12 +4,13 @@
 (require "../vyper.rkt")
 
 
-(define swap-state (vyper-create-final '(a b c d e)
-                                            '((public ()
-                                                      (define tmp a)
-                                                      (modify a tmp)
-                                                      (modify e tmp)
-                                                      (drop tmp)
-                                                      ))))
+(define swap-state (vyper-create-final
+                    '(a b c d e)
+                    '((public ()
+                              (define tmp a)
+                              (modify a e)
+                              (modify e tmp)
+                              (drop tmp)
+                              ))))
 
 (opcodes->size (contract->opcodes swap-state))
